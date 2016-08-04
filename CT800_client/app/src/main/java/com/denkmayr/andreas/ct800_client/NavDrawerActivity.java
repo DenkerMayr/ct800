6d885c51224dcb3f5f9ee49b31d75f2c1149c575
DenkerMayr <andreas.denkmayr@gmail.com>
2016-08-04 06:37:13 +0200

    Added the CowFragment

--- CT800_client/app/src/main/java/com/denkmayr/andreas/ct800_client/NavDrawerActivity.java
+++ CT800_client/app/src/main/java/com/denkmayr/andreas/ct800_client/NavDrawerActivity.java
@@ -1,10 +1,10 @@
 package com.denkmayr.andreas.ct800_client;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class NavDrawerActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, CowFragment.OnFragmentInteractionListener {
+
+    CowFragment cowFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,8 @@ public class NavDrawerActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        cowFragment = CowFragment.newInstance();
     }
 
     @Override
@@ -83,7 +88,7 @@ public class NavDrawerActivity extends AppCompatActivity
         if (id == R.id.nav_info) {
 
         } else if (id == R.id.nav_cows) {
-
+            getSupportFragmentManager().beginTransaction().replace(R.id.mainFraPlace, cowFragment).commit();
         } else if (id == R.id.nav_inspections) {
 
         } else if (id == R.id.nav_import) {
@@ -95,5 +100,10 @@ public class NavDrawerActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
